3c044ec296fb283d7810e29f6c320dfce497a59f
Chase Lambert <theicfire@gmail.com>
2019-04-20 05:03:24 +0000

    Format full document

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,5 +1,5 @@
 /*
-   Here's a small IPv4 example: it asks for a portbase and a destination and 
+   Here's a small IPv4 example: it asks for a portbase and a destination and
    starts sending packets to that destination.
 */
 
@@ -44,54 +44,54 @@ struct PacketPayload {
 };
 
 class output_checker
-   {
-   public:
-      void operator()(size_t block, size_t /*max_blocks*/,
-                      const byte buf[], size_t size)
-         {
-         for(size_t i = 0; i != size; ++i)
-            {
-            byte expected = block*size + i;
-            if(buf[i] != expected)
-               printf("block=%d i=%d got=%02X expected=%02X\n",
-                      (int)block, (int)i, buf[i], expected);
+{
+public:
+	void operator()(size_t block, size_t /*max_blocks*/,
+		const byte buf[], size_t size)
+	{
+		for (size_t i = 0; i != size; ++i)
+		{
+			byte expected = block * size + i;
+			if (buf[i] != expected)
+				printf("block=%d i=%d got=%02X expected=%02X\n",
+				(int)block, (int)i, buf[i], expected);
 
-            }
-         }
-   };
+		}
+	}
+};
 
 class save_to_map
-   {
-   public:
-	   save_to_map(RTPSession& yay): sess(yay) { }
+{
+public:
+	save_to_map(RTPSession& yay) : sess(yay) { }
 
-      void operator()(size_t block_no, size_t,
-                      const byte share[], size_t len)
-         {
-		  int status;
-		  PacketPayload payload;
-		  payload.fec_block_no = block_no;
-		  memcpy(payload.data, share, len); // TODO may be slow...
-		  payload.packet_no = 1;
-		  // TODO this void* &payload is a bit sketch.. is the array memory correct?
-		  //printf("Sending packet that is %zu long\n", sizeof(PacketPayload));
-		  printf("sending fec block %d\n", payload.fec_block_no);
-		  status = sess.SendPacket((void*)&payload, sizeof(PacketPayload), 0, false, 10);
-		  checkerror(status);
-         }
-   private:
-	  RTPSession& sess;
-   };
+	void operator()(size_t block_no, size_t,
+		const byte share[], size_t len)
+	{
+		int status;
+		PacketPayload payload;
+		payload.fec_block_no = block_no;
+		memcpy(payload.data, share, len); // TODO may be slow...
+		payload.packet_no = 1;
+		// TODO this void* &payload is a bit sketch.. is the array memory correct?
+		//printf("Sending packet that is %zu long\n", sizeof(PacketPayload));
+		printf("sending fec block %d\n", payload.fec_block_no);
+		status = sess.SendPacket((void*)& payload, sizeof(PacketPayload), 0, false, 10);
+		checkerror(status);
+	}
+private:
+	RTPSession& sess;
+};
 
 
 void send_sample(std::vector<uint8_t>& input, size_t k, size_t n, RTPSession& sess)
-   {
+{
 
-   fecpp::fec_code fec(k, n);
-   save_to_map saver(sess);
+	fecpp::fec_code fec(k, n);
+	save_to_map saver(sess);
 
-   fec.encode(&input[0], input.size(), std::tr1::ref(saver));
-   }
+	fec.encode(&input[0], input.size(), std::tr1::ref(saver));
+}
 
 
 unsigned long long getMicroseconds()
@@ -120,35 +120,35 @@ int main(void)
 {
 #ifdef RTP_SOCKETTYPE_WINSOCK
 	WSADATA dat;
-	WSAStartup(MAKEWORD(2,2),&dat);
+	WSAStartup(MAKEWORD(2, 2), &dat);
 #endif // RTP_SOCKETTYPE_WINSOCK
-	
+
 	RTPSession sess;
 	uint16_t portbase;
 	std::string ipstr;
-	int status,i,num;
+	int status, i, num;
 
 	std::cout << "Using version " << RTPLibraryVersion::GetVersion().GetVersionString() << std::endl;
 
 	// First, we'll ask for the necessary information
-		
+
 	portbase = 9000;
-	
+
 	// Now, we'll create a RTP session, set the destination, send some
 	// packets and poll for incoming data.
-	
+
 	RTPUDPv4TransmissionParams transparams;
 	RTPSessionParams sessparams;
-	
+
 	// IMPORTANT: The local timestamp unit MUST be set, otherwise
 	//            RTCP Sender Report info will be calculated wrong
 	// In this case, we'll be sending 10 samples each second, so we'll
 	// put the timestamp unit to (1.0/10.0)
-	sessparams.SetOwnTimestampUnit(1.0/10.0);		
-	
+	sessparams.SetOwnTimestampUnit(1.0 / 10.0);
+
 	sessparams.SetAcceptOwnPackets(true);
 	transparams.SetPortbase(portbase);
-	status = sess.Create(sessparams,&transparams);	
+	status = sess.Create(sessparams, &transparams);
 	checkerror(status);
 
 	bool wait_for_first_packet = true;
@@ -162,7 +162,7 @@ int main(void)
 
 				if ((packet = sess.GetNextPacket()) != 0)
 				{
-					const RTPSourceData *source =  sess.GetCurrentSourceInfo();
+					const RTPSourceData* source = sess.GetCurrentSourceInfo();
 					const RTPAddress* addr = source->GetRTPDataAddress();
 					status = sess.AddDestination(*addr);
 					checkerror(status);
@@ -179,10 +179,10 @@ int main(void)
 	printf("Now sending\n");
 	size_t k = 200;
 	size_t n = 255;
-   std::vector<byte> input(k * 1300);
-   for (size_t i = 0; i != input.size(); ++i) {
-	   input[i] = i;
-   }
+	std::vector<byte> input(k * 1300);
+	for (size_t i = 0; i != input.size(); ++i) {
+		input[i] = i;
+	}
 	send_sample(input, k, n, sess);
 	unsigned long long start = getMicroseconds();
 
@@ -193,7 +193,7 @@ int main(void)
 		PacketPayload payload;
 		payload.packet_no = j;
 		sprintf(payload.data, "Thing: %d", j);
-		status = sess.SendPacket((void *)payload.data,sizeof(PacketPayload),0,false,10);
+		status = sess.SendPacket((void*)payload.data, sizeof(PacketPayload), 0, false, 10);
 		checkerror(status);
 		long diff = (getMicroseconds() - start);
 
@@ -202,10 +202,10 @@ int main(void)
 			//usleep(1000);
 		}
 	}
-		
 
-	
-	sess.BYEDestroy(RTPTime(10,0),0,0);
+
+
+	sess.BYEDestroy(RTPTime(10, 0), 0, 0);
 
 #ifdef RTP_SOCKETTYPE_WINSOCK
 	WSACleanup();
